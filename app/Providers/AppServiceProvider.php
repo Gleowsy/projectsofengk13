9ae0e89a83e058a1523cc3e766e57f0512e55636
Gleowsy <notseishiz@gmail.com>
2026-06-11 16:41:52 +0700

    ubahappseriveprovider lagi

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
