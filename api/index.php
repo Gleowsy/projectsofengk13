ea58bf268e76dd43fc2bec31ddc784dd4eaedb32
Gleowsy <notseishiz@gmail.com>
2026-09-20 20:27:14 +0700

    anjay

--- api/index.php
+++ api/index.php
@@ -1,21 +1,4 @@
 <?php
 
-
-$directories = [
-    '/tmp/storage/framework/views',
-    '/tmp/storage/framework/cache',
-    '/tmp/storage/framework/cache/data',
-    '/tmp/storage/framework/sessions',
-];
-
-foreach ($directories as $dir) {
-    if (!file_exists($dir)) {
-        mkdir($dir, 0777, true);
-    }
-}
-
-
-putenv('APP_STORAGE=/tmp/storage');
-
-
+// Memuat aplikasi Laravel secara langsung dari folder public
 require __DIR__ . '/../public/index.php';
